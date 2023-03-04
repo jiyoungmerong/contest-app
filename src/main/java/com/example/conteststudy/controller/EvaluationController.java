8404c9ba76fccfa0a9536879d6cbc106ee0f463b
gks1209 <gks12090607@gmail.com>
2023-03-04 23:11:39 +0900

    evaluationController append

--- src/main/java/com/example/conteststudy/controller/EvaluationController.java
+++ src/main/java/com/example/conteststudy/controller/EvaluationController.java
@@ -1,6 +1,11 @@
 package com.example.conteststudy.controller;
 
+import com.example.conteststudy.domain.dto.EvaluationDto;
+import com.example.conteststudy.domain.dto.UserDto;
 import com.example.conteststudy.service.EvaluationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class EvaluationController {
 
@@ -8,6 +13,12 @@ public class EvaluationController {
 
     public EvaluationController(EvaluationService evaluationService) {
         this.evaluationService = evaluationService;
+    }
+
+    @PostMapping("/evaluations/save")
+    public ResponseEntity<String> join(@RequestBody EvaluationDto evaluationDto) {
+        evaluationService.save(evaluationDto);
+        return ResponseEntity.ok("save success");
     }
 
 
