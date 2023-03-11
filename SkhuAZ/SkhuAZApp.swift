0e8a307af38d6b2fc1a9b58d50f5bc512bb0df96
ParkSY0919 <qhr498@gmail.com>
2023-03-11 15:19:49 +0900

    Initial Commit

---
+++ SkhuAZ/SkhuAZApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkhuAZApp.swift
+//  SkhuAZ
+//
+//  Created by 박신영 on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct SkhuAZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
