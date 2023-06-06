db386c38847e1786841b22d3f5c4746bc1eaf3ba
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-07 02:22:18 +0900

    로그인 응답 수정

--- contest_app/src/main/java/com/example/contest_app/domain/response/LoginResponse.java
+++ contest_app/src/main/java/com/example/contest_app/domain/response/LoginResponse.java
@@ -1,40 +1,20 @@
 package com.example.contest_app.domain.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginResponse {
-    private String sessionId;
 
-    private String nickname;
+    private String email;
+    
+    private String accessToken;
 
-    private String major1;
-
-    private String major2;
-
-    private String semester;
-
-    private boolean department; // 학부
-
-    private String email; // 이메일. 얘가 아이디가 될 거임
-
-    private String password; // 비밀번호
-
-    private boolean graduate; // 졸업 여부
-
-    private boolean major_minor; // 주 부 전공
-
-    private boolean double_major; // 복수전공
-
-    private boolean isLogin;
-
-    private String message; // 로그인 메세지
+    public static LoginResponse of(String email, String accessToken){
+        return new LoginResponse(email, accessToken);
+    }
 
 
 }
