4e161565f4f9cd7b52452d46e5a2461f566cb42d
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-07 02:22:10 +0900

    jwt 기반 회원가입, 로그인 서비스 수정

--- contest_app/src/main/java/com/example/contest_app/service/UserService.java
+++ contest_app/src/main/java/com/example/contest_app/service/UserService.java
@@ -1,5 +1,8 @@
 package com.example.contest_app.service;
+import com.example.contest_app.domain.dto.TokenDto;
 import com.example.contest_app.exception.UserNotFoundException;
+import com.example.contest_app.jwt.TokenProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -22,41 +25,44 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final TokenProvider tokenProvider;
+
+
 
     public void save(UserDto userDto) {
         String encodedPassword = passwordEncoder.encode(userDto.getPassword()); // 패스워드 인코딩
-        userDto.setPassword(encodedPassword);
 
-        User user = new User();
-        user.setEmail(userDto.getEmail());
-        user.setPassword(encodedPassword); // 인코딩된 패스워드 저장
-        user.setNickname(userDto.getNickname());
-        user.setSemester(userDto.getSemester());
-        user.setGraduate(userDto.isGraduate());
-        user.setMajor1(userDto.getMajor1());
-        user.setMajor2(userDto.getMajor2());
-        user.setDepartment(userDto.isDepartment());
-        user.setMajor_minor(userDto.isMajor_minor());
-        user.setDouble_major(userDto.isDouble_major());
+        User user = User.builder()
+                .email(userDto.getEmail())
+                .password(encodedPassword)
+                .nickname(userDto.getNickname())
+                .semester(userDto.getSemester())
+                .graduate(userDto.isGraduate())
+                .major1(userDto.getMajor1())
+                .major2(userDto.getMajor2())
+                .department(userDto.isDepartment())
+                .major_minor(userDto.isMajor_minor())
+                .double_major(userDto.isDouble_major())
+                .build();
 
         userRepository.save(user);
     }
-
 
     public void save(User user) {
         userRepository.save(user);
     }
-
 
     public User findByEmail(String email){
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다." + email));
     }
 
-
-
     public boolean checkDuplicateNickname(String username) {
         return userRepository.findByNickname(username).isPresent();
+    }
+
+    public boolean checkDuplicateEmail(String email){
+        return userRepository.findByEmail(email).isPresent();
     }
 
 
@@ -77,5 +83,19 @@ public class UserService {
         userRepository.delete(user.get());
     }
 
+    public TokenDto login(String email, String password) {
+        // 1. ID/PW 를 기반으로 Authentication 객체 생성
+        // authentication 객체는 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
 
+        //2. 실제 검증(사용자 비밀번호 체크)이 이루어지는 부분
+        // authenticate 매서드가 실행될 때 JwtUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
+//        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증된 정보를 기반으로 JWT 토큰 생성
+        TokenDto tokenDTO = tokenProvider.generateToken(authenticationToken);
+
+        // 4. 로그인 성공하면 토큰DTO에 제대로 들어감
+        return tokenDTO;
+    }
 }
