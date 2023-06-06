25b4b28b6f10c6f916dccf1d38883919fd03b58b
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-07 02:21:38 +0900

    성공 메세지 enum

--- contest_app/src/main/java/com/example/contest_app/status/SuccessStatus.java
+++ contest_app/src/main/java/com/example/contest_app/status/SuccessStatus.java
@@ -1,2 +1,22 @@
-package com.example.contest_app.status;public class SuccessStatus {
+package com.example.contest_app.status;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessStatus {
+
+    /*
+    user
+     */
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료되었습니다."),
+
+    LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공!")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
 }
