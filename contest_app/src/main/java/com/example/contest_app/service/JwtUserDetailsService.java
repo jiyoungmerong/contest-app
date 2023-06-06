8bf1c66a731a62b3379457c91f3a65b30ad26d9f
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-07 02:21:55 +0900

    jwt service

--- contest_app/src/main/java/com/example/contest_app/service/JwtUserDetailsService.java
+++ contest_app/src/main/java/com/example/contest_app/service/JwtUserDetailsService.java
@@ -1,2 +1,40 @@
-package com.example.contest_app.service;public class JwtUserDetailsService {
+package com.example.contest_app.service;
+
+import com.example.contest_app.domain.User;
+import com.example.contest_app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+    }
+
+    // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
+    private UserDetails createUserDetails(User user) {
+        return User.builder()
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .nickname(user.getNickname())
+                .semester(user.getSemester())
+                .graduate(user.isGraduate())
+                .major1(user.getMajor1())
+                .major2(user.getMajor2())
+                .department(user.isDepartment())
+                .major_minor(user.isMajor_minor())
+                .double_major(user.isDouble_major())
+                .routeInfo(user.getRouteInfo())
+                .build();
+    }
 }
