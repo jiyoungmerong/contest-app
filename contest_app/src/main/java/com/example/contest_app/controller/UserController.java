12ba2b9909a69cef24d66733003d5df8d8b77510
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-07 19:49:09 +0900

    회원탈퇴, 정보수정 시 로그인 api 수정

--- contest_app/src/main/java/com/example/contest_app/controller/UserController.java
+++ contest_app/src/main/java/com/example/contest_app/controller/UserController.java
@@ -42,26 +42,28 @@ public class UserController {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     private final TokenProvider tokenProvider;
 
 
     @PostMapping("/users/new-user") // 회원가입
     public ApiResponseDto<String> join(@Valid @RequestBody UserDto userDto) { // @Valid 어노테이션 추가
         try{
-            if(userService.checkDuplicateEmail(userDto.getEmail())){
+            if(userService.checkDuplicateEmail(userDto.getEmail())){ // 이메일 중복시
                 return ApiResponseDto.error(ErrorStatus.CONFLICT_EMAIL_EXCEPTION);
             }
-            else if(userService.checkDuplicateNickname(userDto.getNickname())){
+            else if(userService.checkDuplicateNickname(userDto.getNickname())){ // 닉네임 중복시
                 return ApiResponseDto.error(ErrorStatus.CONFLICT_NICKNAME_EXCEPTION);
             }
             userService.save(userDto);
-            return ApiResponseDto.success(SuccessStatus.SIGNUP_SUCCESS);
-        } catch (Exception e){
+            return ApiResponseDto.success(SuccessStatus.SIGNUP_SUCCESS); // 회원가입 성공
+        } catch (Exception e){ // 그 밖의 예외 발생시
             return ApiResponseDto.error(ErrorStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @PostMapping("/login")
+    @PostMapping("/login") // 로그인
     @ResponseStatus(HttpStatus.OK)
     public ApiResponseDto<TokenDto> login(@RequestBody @Valid final loginRequest request){
         TokenDto tokenDto = userService.login(request.getEmail(), request.getPassword());
@@ -69,62 +71,41 @@ public class UserController {
         user.updateLoginStatus(true);
         userService.save(user);
         return ApiResponseDto.success(SuccessStatus.LOGIN_SUCCESS, tokenDto);
-    } // 추후 구현 : isLogin 상태 true로 업데이트
+    }
 
-//    @PostMapping("/users/login") // 로그인
-//    public Map<String, Object> login(@RequestBody loginRequest request, HttpSession httpSession, HttpServletResponse response) {
-//        User user = userService.findByEmail(request.getEmail());
+    @GetMapping("/checkDuplicate/{nickname}") // 닉네임 중복 확인. 중복이면 true, 아니면 false
+    public boolean checkDuplicateNickname(@PathVariable final String nickname) {
+        return userService.checkDuplicateNickname(nickname);
+    }
 
+//    @GetMapping("/check") // 회원탈퇴, 정보 수정 시 로그인
+//    public ResponseEntity<Boolean> checkLogin(@RequestParam String email, @RequestParam String password) {
+//        Optional<User> optionalUser = userRepository.findByEmail(email);
 //
-//        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-//            responseBody.put("status", "error");
-//            responseBody.put("message", "Invalid password");
-//            return responseBody;
+//        if (!optionalUser.isPresent()) {
+//            return ResponseEntity.ok(false); // 유저가 없을 때
 //        }
 //
-//        user.setLogin(true); // 로그인 상태 업데이트
-//        userService.save(user); // 변경된 로그인 상태 저장
+//        User foundUser = optionalUser.get();
+//        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        if (!passwordEncoder.matches(password, foundUser.getPassword())) {
+//            return ResponseEntity.ok(false); // 비밀번호 틀렸을 때
+//        }
 //
-//
-//        responseBody.put("sessionId", httpSession.getId());
-//        responseBody.put("nickname", user.getNickname());
-//        responseBody.put("major1", user.getMajor1());
-//        responseBody.put("major2", user.getMajor2());
-//        responseBody.put("semester", user.getSemester());
-//        responseBody.put("department", user.isDepartment());
-//        responseBody.put("email", user.getEmail());
-//        responseBody.put("password", user.getPassword());
-//        responseBody.put("graduate", user.isGraduate());
-//        responseBody.put("major_minor", user.isMajor_minor());
-//        responseBody.put("double_major", user.isDouble_major());
-//        responseBody.put("login", user.isLogin());
-//        responseBody.put("message", "Login Success");
-//
-//        return responseBody;
+//        return ResponseEntity.ok(true); // 이메일, 비번 다 맞을 때
 //    }
 
-    @GetMapping("/checkDuplicate/{nickname}") // 닉네임 중복 확인
-    public boolean checkDuplicateNickname(@PathVariable String nickname) {
-        return userService.checkDuplicateNickname(nickname);
-    } // 중복이면 true, 아니면 false
-
-
-    @GetMapping("/check-login")
-    public ResponseEntity<Boolean> checkLogin(@RequestParam String email, @RequestParam String password) {
-        Optional<User> optionalUser = userRepository.findByEmail(email);
-
-        if (!optionalUser.isPresent()) {
-            return ResponseEntity.ok(false); // 유저가 없을 때
+    @GetMapping("/check") // 회원탈퇴, 정보 수정 시 로그인
+    public ApiResponseDto<String> checkLogin(@RequestParam String email, @RequestParam String password){
+        Optional<User> user = userRepository.findByEmail(email); // 이메일에 해당하는 유저 가져오기
+        if(user.isEmpty() || !passwordEncoder.matches(password, user.get().getPassword())){
+            // 이메일, 비번에 해당하는 유저 없을 때
+            return ApiResponseDto.error(ErrorStatus.USER_CERTIFICATION_FAILED);
         }
+        return ApiResponseDto.success(SuccessStatus.CERTIFICATION_SUCCESS);
 
-        User foundUser = optionalUser.get();
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        if (!passwordEncoder.matches(password, foundUser.getPassword())) {
-            return ResponseEntity.ok(false); // 비밀번호 틀렸을 때
-        }
-
-        return ResponseEntity.ok(true); // 이메일, 비번 다 맞을 때
     }
+
 
     @DeleteMapping("/delete") // 회원탈퇴
     public ResponseEntity<String> deleteUser(HttpSession httpSession) {
