d551e5e913514dfeffdc0b6777b9f05f4c0825f2
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-07 19:49:33 +0900

    성공 메시지 추가

--- contest_app/src/main/java/com/example/contest_app/status/SuccessStatus.java
+++ contest_app/src/main/java/com/example/contest_app/status/SuccessStatus.java
@@ -14,7 +14,9 @@ public enum SuccessStatus {
      */
     SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료되었습니다."),
 
-    LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공!")
+    LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공!"),
+
+    CERTIFICATION_SUCCESS(HttpStatus.OK, "유저 인증 성공")
     ;
 
     private final HttpStatus httpStatus;
