a36b9f48088bd23441a4f8bca236e7efefac23f3
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-19 23:26:07 +0900

    회원탈퇴 api 수정

--- contest_app/src/main/java/com/example/contest_app/controller/UserController.java
+++ contest_app/src/main/java/com/example/contest_app/controller/UserController.java
@@ -3,26 +3,19 @@ package com.example.contest_app.controller;
 import com.example.contest_app.domain.User;
 import com.example.contest_app.domain.dto.TokenDto;
 import com.example.contest_app.domain.dto.UserDto;
-import com.example.contest_app.domain.request.EditRequest;
 import com.example.contest_app.domain.request.loginRequest;
 import com.example.contest_app.domain.response.ApiResponseDto;
-import com.example.contest_app.domain.response.EditResponse;
-import com.example.contest_app.domain.response.LoginResponse;
 import com.example.contest_app.domain.response.LogoutResponse;
 
-import com.example.contest_app.jwt.TokenProvider;
 import com.example.contest_app.repository.UserRepository;
 import com.example.contest_app.service.UserService;
 import com.example.contest_app.status.ErrorStatus;
 import com.example.contest_app.status.SuccessStatus;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,8 +24,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -44,34 +35,40 @@ public class UserController {
 
     private final PasswordEncoder passwordEncoder;
 
-    private final TokenProvider tokenProvider;
-
-
     @PostMapping("/users/new-user") // 회원가입
     public ApiResponseDto<String> join(@Valid @RequestBody UserDto userDto) { // @Valid 어노테이션 추가
-        try{
-            if(userService.checkDuplicateEmail(userDto.getEmail())){ // 이메일 중복시
+        try {
+            if (userService.checkDuplicateEmail(userDto.getEmail())) { // 이메일 중복시
                 return ApiResponseDto.error(ErrorStatus.CONFLICT_EMAIL_EXCEPTION);
-            }
-            else if(userService.checkDuplicateNickname(userDto.getNickname())){ // 닉네임 중복시
+            } else if (userService.checkDuplicateNickname(userDto.getNickname())) { // 닉네임 중복시
                 return ApiResponseDto.error(ErrorStatus.CONFLICT_NICKNAME_EXCEPTION);
             }
             userService.save(userDto);
             return ApiResponseDto.success(SuccessStatus.SIGNUP_SUCCESS); // 회원가입 성공
-        } catch (Exception e){ // 그 밖의 예외 발생시
+        } catch (Exception e) { // 그 밖의 예외 발생시
             return ApiResponseDto.error(ErrorStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/login") // 로그인
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponseDto<TokenDto> login(@RequestBody @Valid final loginRequest request){
-        TokenDto tokenDto = userService.login(request.getEmail(), request.getPassword());
+    public ApiResponseDto<TokenDto> login(@RequestBody @Valid final loginRequest request) {
         User user = userService.findByEmail(request.getEmail());
+        if (user == null) {
+            return ApiResponseDto.error(ErrorStatus.USER_CERTIFICATION_FAILED);
+        }
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return ApiResponseDto.error(ErrorStatus.USER_NOT_JOIN);
+        }
+
+        // 비밀번호가 일치하는 경우에만 로그인 처리
+        TokenDto tokenDto = userService.login(request.getEmail(), request.getPassword());
         user.updateLoginStatus(true);
         userService.save(user);
         return ApiResponseDto.success(SuccessStatus.LOGIN_SUCCESS, tokenDto);
     }
+
 
     @GetMapping("/checkDuplicate/{nickname}") // 닉네임 중복 확인. 중복이면 true, 아니면 false
     public boolean checkDuplicateNickname(@PathVariable final String nickname) {
@@ -96,23 +93,32 @@ public class UserController {
 //    }
 
     @GetMapping("/check") // 회원탈퇴, 정보 수정 시 로그인
-    public ApiResponseDto<String> checkLogin(@RequestParam String email, @RequestParam String password){
+    public ApiResponseDto<String> checkLogin(@RequestParam String email, @RequestParam String password) {
         Optional<User> user = userRepository.findByEmail(email); // 이메일에 해당하는 유저 가져오기
-        if(user.isEmpty() || !passwordEncoder.matches(password, user.get().getPassword())){
+        if (user.isEmpty() || !passwordEncoder.matches(password, user.get().getPassword())) {
             // 이메일, 비번에 해당하는 유저 없을 때
             return ApiResponseDto.error(ErrorStatus.USER_CERTIFICATION_FAILED);
         }
         return ApiResponseDto.success(SuccessStatus.CERTIFICATION_SUCCESS);
-
     }
 
+//    @DeleteMapping("/delete") // 회원탈퇴
+//    public ResponseEntity<String> deleteUser(HttpSession httpSession) {
+//        User user = (User) httpSession.getAttribute("user");
+//        try {
+//            userService.deleteUser(user.getEmail());
+//            httpSession.invalidate(); // 세션 무효화
+//            return ResponseEntity.ok("탈퇴가 완료되었습니다.");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("알 수 없는 오류가 발생했습니다.");
+//        }
+//    }
 
-    @DeleteMapping("/delete") // 회원탈퇴
-    public ResponseEntity<String> deleteUser(HttpSession httpSession) {
-        User user = (User) httpSession.getAttribute("user");
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteUser(@AuthenticationPrincipal UserDetails userDetails) {
         try {
-            userService.deleteUser(user.getId());
-            httpSession.invalidate(); // 세션 무효화
+            String email = userDetails.getUsername();
+            userService.deleteUser(email);
             return ResponseEntity.ok("탈퇴가 완료되었습니다.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("알 수 없는 오류가 발생했습니다.");
@@ -151,31 +157,14 @@ public class UserController {
         }
     }
 
-    @PutMapping("/retouch-users") // 유저 정보 수정
-    public ResponseEntity<EditResponse> updateUser(@RequestBody EditRequest editRequest, HttpSession session) {
-        User user = (User) session.getAttribute("user");
-        if (user != null) {
-            user.setNickname(editRequest.getNickname());
-            user.setSemester(editRequest.getSemester());
-            user.setGraduate(editRequest.isGraduate());
-            user.setDepartment(editRequest.isDepartment());
-            user.setMajor1(editRequest.getMajor1());
-            user.setMajor2(editRequest.getMajor2());
-            user.setMajor_minor(editRequest.isMajor_minor());
-            user.setDouble_major(editRequest.isDouble_major());
-
-            userRepository.save(user); // 업데이트된 User 엔티티를 데이터베이스에 저장
-
-            EditResponse userResponse = new EditResponse(user);
-            return ResponseEntity.ok(userResponse);
-        }
-
-        EditResponse e = new EditResponse("로그인이 되어있지 않습니다.");
-        return ResponseEntity.badRequest().body(e);
-    }
-
-
+//    @PutMapping("/retouch-users") // 유저 정보 수정
+//    public ApiResponseDto<EditResponse> updateUser(@RequestBody final EditRequest editRequest, HttpSession session) {
+//        User user = (User) session.getAttribute("user");
+//
+//        if(user == null){
+//            return ApiResponseDto.error(ErrorStatus.NOT_LOGIN_STATUS)
+//        }
+//
+//        return ApiResponseDto.success()
+//    }
 }
-
-
-
